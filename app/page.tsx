444ec9f5cb9e3e21233f99a4b1f4bbed44371eb2
pashalopatuk <pashaplaylopatuk@gmail.com>
2023-09-03 14:47:54 +0300

    Updated query to db

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { client } from "./lib/sanity"
 
 
 async function getData() {
-  const query = '*[_type == "post"]'
+  const query = '*[_type == "post"] | order(_createdAt)'
 
   const data = await client.fetch(query)
 
@@ -27,7 +27,7 @@ export default async function IndexPage() {
 
       <ul className="space-y-4">
         {
-        data.reverse().map( (post) => (
+        data.map( (post) => (
           <li className="" key={post._id}>
             <article className="space-y-2 xl:grid-cols-4 xl:items-baseline xl:space-y-0">
               <div className="">
